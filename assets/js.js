08d7878179efffa90fb177922051258a3e2898ca
kevinhng77 <62964063+kevinhng77@users.noreply.github.com>
2023-04-03 01:11:30 -0700

    trying to add eventlistener to js

---
+++ assets/js.js
@@ -0,0 +1,30 @@
+var timeEl = document.querySelector(".timeLeft");
+var firstQuestion = document.querySelector(".question1");
+var secondQuestion = document.querySelector(".question2");
+var thirdQuestion = document.querySelector(".question3");
+var fourthQuestion = document.querySelector(".question4");
+var allAnswers = document.getElementsByTagName('li');
+var totalScore= 0;
+
+
+function startQuiz(){
+    pass
+}
+
+function timeCountDown(){
+    pass
+}
+
+function quizScore(){
+    allAnswers.addEventListener("click", function(){
+        if (this.getElementsByClassName("correct-answer"));
+            totalScore +=
+    
+        
+    console.log(totalScore);
+    } );
+}
+
+function rightOrWrong(){
+    pass
+}
