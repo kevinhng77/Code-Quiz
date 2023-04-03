502556bb3fb42ac3806a6ef21d48ca230da24fd6
kevinhng77 <62964063+kevinhng77@users.noreply.github.com>
2023-04-03 15:28:58 -0700

    questions

--- assets/js.js
+++ assets/js.js
@@ -1,11 +1,37 @@
 var timeEl = document.querySelector(".timeLeft");
-var firstQuestion = document.querySelector(".question1");
-var secondQuestion = document.querySelector(".question2");
-var thirdQuestion = document.querySelector(".question3");
-var fourthQuestion = document.querySelector(".question4");
-var allAnswers = document.getElementsByTagName('li');
 var totalScore= 0;
+var questions = document.getElementById("questionsList");
+var counter= 0;
 
+var questionsArray = [
+    {
+        question: "question1",
+        answerA: "answerA",
+        answerB: "answerB",
+        answerC: "answerC",
+        answerD: "answerD",
+        answer: "answerA"
+    },
+    {
+        question: "question2",
+        answerA: "answere",
+        answerB: "answerf",
+        answerC: "answerg",
+        answerD: "answerh",
+        answer: "answeri"
+    },
+    {
+        question: "question3",
+        answerA: "answerj",
+        answerB: "answerk",
+        answerC: "answerl",
+        answerD: "answerm",
+        answer: "answern"
+    }
+
+]
+
+console.log(questionsArray[0][2]);
 
 function startQuiz(){
     pass
@@ -16,13 +42,13 @@ function timeCountDown(){
 }
 
 function quizScore(){
-    allAnswers.addEventListener("click", function(){
-        if (this.getElementsByClassName("correct-answer"));
-            totalScore +=
-    
-        
-    console.log(totalScore);
-    } );
+    // allAnswers.addEventListener("click", addScore);
+    pass
+}
+
+
+function addScore(){
+    pass
 }
 
 function rightOrWrong(){
